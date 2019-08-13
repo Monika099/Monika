395bc77aa76c0642082f66061fbe67d180060ea2
Monika099 <54067645+Monika099@users.noreply.github.com>
2019-08-13 18:37:52 +0530

    Create moni.c

---
+++ moni.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter the number");
+scanf(%d",&i);
+if(i>0)
+{
+printf("positive");
+}
+else if(i<0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+return 0;
+}
